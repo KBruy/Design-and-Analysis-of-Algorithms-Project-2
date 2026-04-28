32ce914f57896207cf9a494e1d23b25950ad5172
KBury <krystian.bury1@gmail.com>
2026-04-28 18:05:57 +0200

    feat: measure run() implementation

---
+++ Research.cpp
@@ -0,0 +1,44 @@
+#include "Research.h"
+#include "GraphList.h"
+#include "GraphMatrix.h"
+#include "GraphGenerator.h"
+#include "Dijkstra.h"
+
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <chrono>
+
+using namespace std;
+
+void Research::run() {
+    vector<int> verticesValues = {10,50,100,500,1000};
+    vector<double> densities = {0.25, 0.50, 0.75, 1.00};
+
+    int instanceCount = 100;
+
+    ofstream file("results.csv");
+
+    if (!file.is_open()) {
+        cout << "Nie udalo sie otworzyc pliku results.csv\n";
+        return;
+    }
+
+    file << "representation,vertices,density,instances,average_time_ns\n";
+    cout << "\nPomiar rozpoczety....\n";
+    for (int vertices : verticesValues) {
+        for (double density : densities) {
+            cout << "V = " << vertices << ", gestosc = " << density * 100 << "%\n";
+
+            long long listAverageTime = measureList(vertices, density, instanceCount);
+            long long matrixAverageTime = measureMatrix(vertices, density, instanceCount);
+
+            file << "list,"<<vertices<<","<<density<<","<<instanceCount<<","<<listAverageTime<<"\n";
+            file << "matrix,"<<vertices<<","<<density<<","<<instanceCount<<","<<listAverageTime<<"\n";
+        }
+    }
+
+    file.close();
+
+    cout << "\nBadania zakonczone. Wyniki zapisano do pliku results.csv\n";
+}
